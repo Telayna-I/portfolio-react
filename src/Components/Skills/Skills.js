2111128a689106f14178ce8092ce75df882cab4f
Telayna-I <telayna98@gmail.com>
2023-09-25 20:25:30 -0300

    Nuevas tecnologias

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,69 +1,97 @@
-import React from 'react'
-import { FaHtml5, FaCss3Alt, FaGitAlt, FaReact, FaGithub } from 'react-icons/fa'
-import { IoLogoJavascript, IoLogoSass } from 'react-icons/io'
-import { SiJquery, SiStyledcomponents, SiRedux, SiFirebase, SiBootstrap, SiMysql } from 'react-icons/si'
-import './Skills.css'
+import React from "react";
+import { FaHtml5, FaCss3Alt, FaGitAlt, FaReact, FaGithub } from "react-icons/fa";
+import { IoLogoJavascript, IoLogoSass } from "react-icons/io";
+import {
+    SiJquery,
+    SiStyledcomponents,
+    SiRedux,
+    SiFirebase,
+    SiBootstrap,
+    SiMysql,
+    SiExpress,
+    SiMongodb,
+} from "react-icons/si";
+
+import { DiNodejsSmall } from "react-icons/di";
+import { TbBrandReactNative} from "react-icons/tb";
+import "./Skills.css";
 
 const Skills = () => {
     return (
-        <section id='Skills' className="skills">
+        <section id="Skills" className="skills">
             <h2>Skills</h2>
             <div className="skills-grid-container">
                 <div className="skilli html">
-                    <FaHtml5 className='fab'/>
+                    <FaHtml5 className="fab" />
                     <p>HTML5</p>
                 </div>
                 <div className="skilli css">
-                    <FaCss3Alt className='fab'/>
+                    <FaCss3Alt className="fab" />
                     <p>CSS3</p>
                 </div>
                 <div className="skilli css">
-                    <IoLogoSass className='fab'/>
+                    <IoLogoSass className="fab" />
                     <p>Sass</p>
                 </div>
                 <div className="skilli css">
-                    <SiBootstrap className='fab'/>
+                    <SiBootstrap className="fab" />
                     <p>Bootstrap</p>
                 </div>
                 <div className="skilli git">
-                    <FaGitAlt className='fab'/>
+                    <FaGitAlt className="fab" />
                     <p>Git</p>
                 </div>
                 <div className="skilli git">
-                    <FaGithub className='fab'/>
+                    <FaGithub className="fab" />
                     <p>Git-Hub</p>
                 </div>
                 <div className="skilli javascript">
-                    <IoLogoJavascript className='fab'/>
+                    <IoLogoJavascript className="fab" />
                     <p>JavaScript</p>
                 </div>
                 <div className="skilli">
-                    <SiJquery className='fab'/>
+                    <SiJquery className="fab" />
                     <p>Jquery</p>
                 </div>
                 <div className="skilli">
-                    <SiFirebase className='fab'/>
+                    <DiNodejsSmall className="fab" />
+                    <p>NodeJs</p>
+                </div>
+                <div className="skilli">
+                    <SiExpress className="fab" />
+                    <p>ExpressJs</p>
+                </div>
+                <div className="skilli">
+                    <SiMongodb className="fab" />
+                    <p>MongoDb</p>
+                </div>
+                <div className="skilli">
+                    <SiFirebase className="fab" />
                     <p>Firebase</p>
                 </div>
                 <div className="skilli">
-                    <SiMysql className='fab sql'/>
+                    <SiMysql className="fab sql" />
                     <p>MySQL</p>
                 </div>
                 <div className="skilli gitHub">
-                    <FaReact className='fab'/>
+                    <FaReact className="fab" />
                     <p>React Js</p>
                 </div>
                 <div className="skilli gitHub">
-                    <SiRedux className='fab'/>
+                    <TbBrandReactNative className="fab" />
+                    <p>React Native</p>
+                </div>
+                <div className="skilli gitHub">
+                    <SiRedux className="fab" />
                     <p>Redux</p>
                 </div>
                 <div className="skilli">
-                    <SiStyledcomponents className='fab styled'/>
+                    <SiStyledcomponents className="fab styled" />
                     <p>Styled Components</p>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Skills
+export default Skills;
